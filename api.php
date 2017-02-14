fe94bd2bc497c3f0296fc485da9eeacb88d686bf
melnikdev <melnikdev@gmail.com>
2017-02-15 00:05:03 +0200

    master test

--- api.php
+++ api.php
@@ -8,6 +8,7 @@ $url = explode('/', trim($_SERVER['PATH_INFO'], '/'));
 $method = $_SERVER['REQUEST_METHOD'];
 
 //$db = new PDO('sqlite:../../modules/system/html/openserver/adminer/database.db');
+//tester 22
 $db = new PDO('sqlite:dbTodo.db');
 
 $api = new RestApi($url, $method, $db);
